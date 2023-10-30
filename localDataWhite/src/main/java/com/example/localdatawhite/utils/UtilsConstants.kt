042480ee3defd8565b35f185e320d1168096036e
Mikhail <mikhail.bogdanov.w@gmail.com>
2023-10-30 18:35:37 +0300

    -refactor code

--- localDataWhite/src/main/java/com/example/localdatawhite/utils/UtilsConstants.kt
+++ localDataWhite/src/main/java/com/example/localdatawhite/utils/UtilsConstants.kt
@@ -4,6 +4,6 @@ object UtilsConstants {
 
     const val DEFAULT_LOCALE = "en"
     const val DEFAULT_DARK_THEME = false
-    const val DEFAULT_ACCEPTING_REQUIRED = true
+    const val DEFAULT_ACCEPTING_REQUIRED = true //TODO SET ACCORDING TO HOST STATE
 
 }
