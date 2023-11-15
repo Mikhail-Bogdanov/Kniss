cb5c2febe60c2a60aa070c058407d21df9afd0b4
Mikhail <mikhail.bogdanov.w@gmail.com>
2023-11-15 18:03:44 +0300

    -refactor code -fixed version increaser bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,17 @@ plugins {
     id(Dependencies.Jvm.Jvm) version Dependencies.Jvm.JvmVersion apply false
 }
 
-//task("addPreCommitGitHookOnBuild") {
-//    println("Running Add Pre Commit Git Hook Script on Build...")
-//    exec {
-//        commandLine("cp", "./.scripts/pre-commit", "./.git/hooks")
-//    }
-//    println("✅ Added Pre Commit Git Hook Script")
-//}
-//
-//task("setAppVersionOnBuild") {
-//    //setting app version from TXT file
-//    val currentVersion = File("./.scripts/AppVersion.txt").readText()
-//    Settings.VersionName = currentVersion
-//    println("Current App Version: ${Settings.VersionName}")
-//}
+task("addPreCommitGitHookOnBuild") {
+    println("Running Add Pre Commit Git Hook Script on Build...")
+    exec {
+        commandLine("cp", "./.scripts/pre-commit", "./.git/hooks")
+    }
+    println("✅ Added Pre Commit Git Hook Script")
+}
+
+task("setAppVersionOnBuild") {
+    //setting app version from TXT file
+    val currentVersion = File("./.scripts/AppVersion.txt").readText()
+    Settings.VersionName = currentVersion
+    println("Current App Version: ${Settings.VersionName}")
+}
