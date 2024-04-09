5a55c2f867916f2fb98790ac15fba1189657e6ea
mikhail_work <mikhail.bogdanov.w@gmail.com>
2024-04-09 18:41:16 +0300

    -updated navigation

---
+++ presentation/src/main/java/com/qwertyuiop/presentation/ui/utils/navigation/NavGraphs.kt
@@ -0,0 +1,21 @@
+package com.qwertyuiop.presentation.ui.utils.navigation
+
+import com.qwertyuiop.presentation.ui.composables.destinations.StartDestination
+import com.qwertyuiop.presentation.ui.composables.destinations.KnittingDestination
+import com.qwertyuiop.presentation.ui.composables.destinations.SettingsDestination
+import com.ramcosta.composedestinations.spec.DestinationSpec
+import com.ramcosta.composedestinations.spec.NavGraphSpec
+import com.ramcosta.composedestinations.spec.Route
+
+
+object NavGraphs {
+    val PresentationNavGraph = object : NavGraphSpec {
+        override val destinationsByRoute: Map<String, DestinationSpec<*>> = listOf(
+            StartDestination,
+            KnittingDestination,
+            SettingsDestination
+        ).associateBy { it.route }
+        override val route: String = "presentation_root"
+        override val startRoute: Route = StartDestination
+    }
+}
