9eb7172ae31cbad526e6bc449b823a3081f021c8
mikhail_work <mikhail.bogdanov.w@gmail.com>
2024-04-09 18:40:33 +0300

    -updated koin module

--- presentation/src/main/java/com/qwertyuiop/presentation/di/PresentationModule.kt
+++ presentation/src/main/java/com/qwertyuiop/presentation/di/PresentationModule.kt
@@ -1,9 +1,15 @@
 package com.qwertyuiop.presentation.di
 
+import com.qwertyuiop.presentation.ui.composables.presentation.knitting.mvi.KnittingViewModel
+import com.qwertyuiop.presentation.ui.composables.presentation.settings.mvi.SettingsViewModel
+import com.qwertyuiop.presentation.ui.composables.presentation.start.mvi.StartViewModel
+import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
 object PresentationModule {
     val module = module {
-
+        viewModelOf(::KnittingViewModel)
+        viewModelOf(::StartViewModel)
+        viewModelOf(::SettingsViewModel)
     }
 }
